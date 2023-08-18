564f4418e55e190fdd430b65fc34747c3a50f0fc
agmui <anthonynmui@gmail.com>
2023-08-18 17:16:21 -0400

    updated init script

--- linux_init.sh
+++ linux_init.sh
@@ -1,4 +1,6 @@
-sudo apt install -y  cmake git
+sudo apt install -y  git
+sudo apt install cmake gcc-arm-none-eabi libnewlib-arm-none-eabi build-essential # from pico docs
+
 git submodule update --init --recommend-shallow # updates rm_pico_dev
 cd rm_pico_dev
 git submodule update --init --recommend-shallow # updates rm_pico_dev/lib

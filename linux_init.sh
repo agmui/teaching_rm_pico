dbd3e20ae7f11612474bee530d57a7d5c2b68648
agmui <anthonynmui@gmail.com>
2023-08-18 03:40:14 -0400

    added config files

---
+++ linux_init.sh
@@ -0,0 +1,4 @@
+cd rm_pico_dev
+git submodule update --init --recommened-shallow
+cd lib/pico-sdk/
+git submodule update --init --recommened-shallow
---
+++ main.cpp
@@ -0,0 +1,38 @@
+
+#include <iostream>
+#include <drivers.h>
+#include <Debug.h>
+#include "pico/stdlib.h"
+
+int main(int argc, char const *argv[])
+{
+    pico::Drivers *drivers = new pico::Drivers();
+    // pico::Drivers *drivers = pico::Drivers::getDrivers();
+
+    stdio_init_all();
+    gpio_init(25);
+    gpio_set_dir(25, GPIO_OUT);
+
+    drivers->remote.initialize();
+    while (1)
+    {
+        // Turn On LED
+        gpio_put(25, 1); // Set pin 25 to high
+        sleep_ms(250);
+        printf("LED switched on!\n");
+        // Turn Off LED
+        gpio_put(25, 0); // Set pin 25 to high.
+        sleep_ms(250);
+        printf("LED switched off!\n");
+
+        // ====
+        std::cout << "." << std::endl;
+        drivers->remote.read(); // Reading the remote before we check if it is connected yet or not.
+        if (drivers->remote.isConnected())
+        {
+            std::cout << "remote: " << drivers->remote.getChannel(pico::communication::serial::Remote::Channel::LEFT_HORIZONTAL) << std::endl;
+        }
+        tight_loop_contents();
+    }
+}
+

0f2a5ba6e998865a57da9b4e71ab9708d3a0db36
agmui <anthonynmui@gmail.com>
2023-08-18 14:20:11 -0400

    added wokwi support

--- linux_init.sh
+++ linux_init.sh
@@ -1,5 +1,5 @@
 sudo apt install -y  cmake git
 cd rm_pico_dev
-echo yes | git submodule update --init --recommend-shallow
+git submodule update --init --recommend-shallow 
 cd lib/pico-sdk/
-echo yes | git submodule update --init --recommend-shallow
+git submodule update --init --recommend-shallow

06653300723a595b5c7f2d8a00068078c65adecd
agmui <anthonynmui@gmail.com>
2023-08-18 17:04:05 -0400

    updated init file

--- linux_init.sh
+++ linux_init.sh
@@ -1,5 +1,7 @@
 sudo apt install -y  cmake git
+git submodule update --init --recommend-shallow # updates rm_pico_dev
 cd rm_pico_dev
-git submodule update --init --recommend-shallow 
+git submodule update --init --recommend-shallow # updates rm_pico_dev/lib
 cd lib/pico-sdk/
-git submodule update --init --recommend-shallow
+git submodule update --init --recommend-shallow # updates rm_pico_dev/lib/pico-sdk/lib
+

44ee277cd664456d4f0f564bc0093d051d9d7293
agmui <anthonynmui@gmail.com>
2023-08-18 03:46:04 -0400

    updated script

--- linux_init.sh
+++ linux_init.sh
@@ -1,4 +1,5 @@
+sudo apt install -y  cmake git
 cd rm_pico_dev
-git submodule update --init --recommened-shallow
+echo yes | git submodule update --init --recommend-shallow
 cd lib/pico-sdk/
-git submodule update --init --recommened-shallow
+echo yes | git submodule update --init --recommend-shallow

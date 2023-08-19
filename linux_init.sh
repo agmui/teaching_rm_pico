a6384e26f465b906ce412e625e0684b1c6a59e69
agmui <anthonynmui@gmail.com>
2023-08-19 00:39:58 -0400

    updated script

--- linux_init.sh
+++ linux_init.sh
@@ -8,14 +8,15 @@ git submodule update --init --recommend-shallow # updates rm_pico_dev
 cd rm_pico_dev
 git submodule update --init --recommend-shallow # updates rm_pico_dev/lib and rm_pico_dev/picotool
 cd lib/pico-sdk/
+sdk_path=$(pwd)
 git submodule update --init --recommend-shallow # updates rm_pico_dev/lib/pico-sdk/lib
 
 
 # building picotool
-sudo apt install libusb-1.0-0-dev
+sudo apt install -y libusb-1.0-0-dev
 cd ../../picotool
 mkdir build
 cd build
-export PICO_SDK_PATH=../lib/pico-sdk/
+export PICO_SDK_PATH=$sdk_path
 cmake ../
 make
